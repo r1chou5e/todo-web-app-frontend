2016009163a5629544d02e7f6fcede0ce5a3ffda
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2024-01-24 13:53:45 +0700

    Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/auth/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 module.exports = withMT({
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Roboto Slab', 'serif'],
+      body: ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 });
